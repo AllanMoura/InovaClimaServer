c0751c50fe93ba856174e3fca8e8ce08ebb87d70
Allan Moura <allan.moura16@gmail.com>
2018-08-17 18:28:06 -0300

    descricao dos endpoints no api.php

--- routes/api.php
+++ routes/api.php
@@ -19,13 +19,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 */
 
-Route::get('/', 'HomeController@version')->name('api.index');
-Route::get('/nicknames', "NicknameController@index")->name('nicknames.index');
-Route::post('/nicknames', "NicknameController@register")->name('nicknames.register');
-Route::post('/places', "PlaceController@createPlace")->name('places.create');
-Route::get('/places', "PlaceController@index")->name('places.index');
-Route::post('/places/search', "PlaceController@searchPlaces")->name('places.search');
-Route::post('/favoritos', "FavoritoController@changeFavorito")->name('favoritos.change');
-Route::get('/favoritos/{nicknameId}', "FavoritoController@getFavoritos")->name('favoritos.getFavoritos');
-Route::post('/previsao', "PlaceController@editPrevisoes")->name('previsoes.edit');
-Route::get('/places/{placeId}', "PlaceController@getPlace")->name('places.getPlace');
+Route::get('/', 'HomeController@version')->name('api.index'); // versão
+Route::get('/nicknames', "NicknameController@index")->name('nicknames.index'); //Lista de todos os usuarios
+Route::post('/nicknames', "NicknameController@register")->name('nicknames.register'); //Login/registra, se existir, loga, se não, registra. atributos :> nickname: string
+Route::post('/places', "PlaceController@createPlace")->name('places.create');//Cria um novo place - elemento :> cidade: string, bairro: string
+Route::get('/places', "PlaceController@index")->name('places.index'); //busca todas as places do banco
+Route::post('/places/search', "PlaceController@searchPlaces")->name('places.search'); //busca places que contenham as palavras do elemento query. elementos:> query: string, nicknameId: integer
+Route::post('/favoritos', "FavoritoController@changeFavorito")->name('favoritos.change');//Toggle de favoritos, se estiver como favorito, remove, retorna um  objeto "message", se não, adiciona como favoritos. elementos:> nicknameId: integer, placeId: integer.
+Route::get('/favoritos/{nicknameId}', "FavoritoController@getFavoritos")->name('favoritos.getFavoritos');// funcao get que recebe um nicknameId e retorna a lista de places favoritos do usuario
+Route::post('/previsao', "PlaceController@editPrevisoes")->name('previsoes.edit'); // recebe uma lista de previsoes e atualiza no servidor, retorna a mesma lista de previsoes
+Route::get('/places/{placeId}', "PlaceController@getPlace")->name('places.getPlace');// retorna um place, recebe um placeId como parametro
